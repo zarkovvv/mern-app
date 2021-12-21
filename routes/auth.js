51a071c6fefd998d4fa24c798526f18338e99963
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-12-21 11:32:14 +0200

    initial commit & project setup

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { register, login, forgotpassword, resetpassword } = require('../controllers/auth');
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/forgotpassword").post(forgotpassword);
+router.route("/resetpassword/:token").put(resetpassword);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {console.log(`Server listening on port: ${PORT}`)});
