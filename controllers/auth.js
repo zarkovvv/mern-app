234a4d3de187d700f0a3db6a2fec0df35fc2b96f
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-12-22 08:41:57 +0200

    bug fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,39 +28,34 @@ exports.login = async (req, res, next) => {
             success: false,
             error: 'Please provide email and password!'
         });
-        res.end();
     } else {
         try {
             const user = await User.findOne({ email }).select('+password');
 
             if (!user) {
-                res.status(404).json({
+                return res.status(404).json({
                     success: false,
                     error: 'Invalid user!'
                 });
-                res.end();
             }
 
             const passwordMatch = await user.matchPasswords(password);
             if (!passwordMatch) {
-                res.status(404).json({
+                return res.status(404).json({
                     success: false,
                     error: 'Invalid credentials!'
                 });
-                res.end();
             }
 
             res.status(200).json({
                 success: true,
                 token: '34r2rc2b66458n5gb'
             });
-            res.end();
         } catch (e) {
             res.status(500).json({
                 success: false,
                 error: e.message
             });
-            res.end();
         }
     }
 }
