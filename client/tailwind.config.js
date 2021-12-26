2ec96b432bde954c14ab994291a896611cb8dfd5
zarkovvv <zarcoff@abv.bg>
2021-12-26 23:10:59 +0200

    add custom color

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#121212'
+      },
+    }
   },
   plugins: [],
 }
