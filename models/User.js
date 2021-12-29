9848c3b2d6ea215ad870a46aba36eda2fd74827a
Alexander Zarkov <alexander.zarcoff@outlook.com>
2021-12-29 15:10:26 +0200

    refactor

--- models/User.js
+++ models/User.js
@@ -4,55 +4,55 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema({
-    uid: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: [true, 'please provide a username']
-    },
-    email: {
-        type: String,
-        required: [true, 'please provide an email'],
-        unique: true,
-        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide a valid email']
-    },
-    password: {
-        type: String,
-        required: [true, 'please provide a password'],
-        minlength: 6,
-        select: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpire: Date
+  uid: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: [true, 'please provide a username']
+  },
+  email: {
+    type: String,
+    required: [true, 'please provide an email'],
+    unique: true,
+    match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'please provide a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'please provide a password'],
+    minlength: 6,
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date
 });
 
 UserSchema.pre('save', async function(next) {
-   if (!this.isModified('password')) {
-       next();
-   }
+  if (!this.isModified('password')) {
+    next();
+  }
 
-   const salt = await bcrypt.genSalt(10);
-   this.password = await bcrypt.hash(this.password, salt);
-   next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 UserSchema.methods.matchPasswords = async function(password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 }
 
 UserSchema.methods.getSignedToken = function () {
-    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
+  return jwt.sign({id: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRE});
 }
 
 UserSchema.methods.getResetToken = function () {
-    const resetToken = crypto.randomBytes(20).toString('hex');
+  const resetToken = crypto.randomBytes(20).toString('hex');
 
-    this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-    this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
+  this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000);
 
-    return resetToken;
+  return resetToken;
 }
 
 const User = mongoose.model('User', UserSchema);
