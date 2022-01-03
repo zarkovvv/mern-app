c61d0df8c5bb6d8b29792a5939c8a586a3811601
Alexander Zarkov <alexander.zarcoff@outlook.com>
2022-01-03 16:16:19 +0200

    add update and delete ad functionality

--- controllers/private.js
+++ controllers/private.js
@@ -62,3 +62,63 @@ exports.createAd = async (req, res, next) => {
         next(e);
     }
 }
+
+exports.updateAd = async (req, res, next) => {
+
+    const aid = req.ad.aid;
+    const _id = req.ad._id;
+    const updateDate = Date.now();
+
+    const {title, description, images} = req.body;
+    const {brand, model, km, color, year, price} = req.body.car;
+    const {engineType, power} = req.body.car.engine;
+
+    const ad = {
+        aid: aid,
+        title: title,
+        description: description,
+        postedBy: req.ad.postedBy,
+        createdAt: req.ad.createdAt,
+        lastUpdated: updateDate,
+        images: req.ad.images,
+        car: {
+            brand: brand,
+            model: model,
+            km: km,
+            color: color,
+            year: year,
+            price: price,
+            engine: {
+                engineType: engineType,
+                power: power
+            }
+        }
+    }
+
+    try {
+        const updatedAd = await Ad.findByIdAndUpdate(_id, ad, {new: true});
+
+        res.status(201).json({
+            success: true,
+            data: updatedAd
+        })
+    } catch (e) {
+        next(e);
+    }
+}
+
+exports.deleteAd = async (req, res, next) => {
+
+    const _id = req.ad._id;
+
+    try {
+        const deletedAd = await Ad.findByIdAndDelete(_id);
+
+        res.status(201).json({
+            success: true,
+            data: deletedAd
+        })
+    } catch (e) {
+        next(e);
+    }
+}
