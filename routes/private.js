70be55ac5843c8b185408f59b0ec3dc856d72675
Alexander Zarkov <alexander.zarcoff@outlook.com>
2022-01-03 16:15:22 +0200

    add ad crud routes

--- routes/private.js
+++ routes/private.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {getAds, createAd} = require('../controllers/private');
+const {getAds, createAd, updateAd, deleteAd} = require('../controllers/private');
 const {protect} = require('../middleware/auth');
+const {protectOwnership} = require('../middleware/ads');
 
 // router.route('/').get(protect, getAds);
 router.route('/ads').get(protect, getAds);
-router.route('/ads').post(protect, createAd); // protect creation
+router.route('/ads').post(protect, createAd);
+router.route('/ads/updateAd').post(protect, protectOwnership, updateAd);
+router.route('/ads/deleteAd').post(protect, protectOwnership, deleteAd);
 
 module.exports = router;
