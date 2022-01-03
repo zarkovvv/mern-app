56e433f0a69da23f34d04a91301fd29bf4c310c0
Alexander Zarkov <alexander.zarcoff@outlook.com>
2022-01-03 16:13:21 +0200

    implement logout

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,18 +1,30 @@
-import {AppBar, Container, Grid, Grow, Typography} from "@mui/material";
+import {AppBar, Button, Container, Grid, Grow, Typography} from "@mui/material";
 import carads from '../../assets/images/carads.png';
 import Ads from "../common/Ads/Ads";
 import Form from "../common/Form/Form";
 import useStyles from './styles';
+import {useDispatch} from "react-redux";
+import {logout} from "../../redux/slices/authSlice";
+import {useNavigate} from "react-router-dom";
 
 const Home = () => {
 
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem('authData');
+    navigate('/login');
+  }
 
   return (
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">CarAds</Typography>
         <img className={classes.image} src={carads} alt="CarAds" height="60"/>
+        <Button onClick={handleLogout}>LOGOUT</Button>
       </AppBar>
       <Grow in>
         <Container>
