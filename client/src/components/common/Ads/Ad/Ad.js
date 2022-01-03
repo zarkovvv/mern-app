e90f32849b2f762c5af101b27a7b26ae06c44da5
Alexander Zarkov <alexander.zarcoff@outlook.com>
2022-01-03 16:10:49 +0200

    implement ad component

--- client/src/components/common/Ads/Ad/Ad.js
+++ client/src/components/common/Ads/Ad/Ad.js
@@ -1,16 +1,68 @@
-
 import useStyles from './styles';
+import {Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Typography} from "@mui/material";
+import {Delete, MoreHoriz, ThumbUpAlt} from "@mui/icons-material";
+import {useState} from "react";
+import EditDialog from "../../../dialogs/EditDialog";
+import {useDispatch} from "react-redux";
+import {deleteAd, getAds} from "../../../../redux/slices/adsSlice";
+import {toast} from "react-toastify";
+import Alert from "../../../alerts/Alert";
+import axios from "axios";
 
 const Ad = ({data}) => {
+
+  const dispatch = useDispatch();
+
+  const [openEdit, setOpenEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleOpenEdit = () => {
+    setOpenEdit(true);
+  }
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  }
+
+  const handleDelete = async () => {
+    const {aid} = data;
+    setLoading(true);
+    try {
+      await dispatch(deleteAd({aid})).unwrap();
+      toast.success('Successfully deleted ad!');
+    } catch (e) {
+      toast.error(e.response.data.error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   const classes = useStyles();
 
   return(
     <>
-      <h1>{data.title}</h1>
-      <h2>{data.car.make}</h2>
-      <h2>{data.car.model}</h2>
-      <p>{data.description}</p>
+      <Card className={classes.card}>
+        <CardMedia className={classes.media} image={'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={"title"} />
+        <div className={classes.overlay}>
+          <Typography variant="h6">{data.title}</Typography>
+          <Typography variant="body2">{"moment"}</Typography>
+        </div>
+        <div className={classes.overlay2}>
+          <Button style={{ color: 'white' }} size="small" onClick={handleOpenEdit}><MoreHoriz fontSize="default" /></Button>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{data.title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{data.description}</Typography>
+        </CardContent>
+        <CardActions className={classes.cardActions}>
+          <Button size="small" color="primary" ><ThumbUpAlt fontSize="small" /> Like 1 </Button>
+          <Button size="small" color="primary" onClick={handleDelete}>
+            {loading ? <CircularProgress size={24} /> : <><Delete fontSize="small" /> Delete</>}
+          </Button>
+        </CardActions>
+      </Card>
+      {openEdit && <EditDialog show={true} onClose={handleCloseEdit} ad={data}/>}
+      <Alert />
     </>
   );
 }
--- client/src/components/common/Ads/Ads.js
+++ client/src/components/common/Ads/Ads.js
@@ -1,19 +1,28 @@
 import Ad from "./Ad/Ad";
-import useStyles from "./Ad/styles";
+import useStyles from "./styles";
 import {useSelector} from "react-redux";
+import {CircularProgress, Grid} from "@mui/material";
 
 
 const Ads = () => {
 
   const classes = useStyles();
-  const ads = useSelector((state) => state.ads);
+  const adItems = useSelector((state) => state.ads.items);
 
-  return(
+  return (
     <>
-      <h1>ADS</h1>
-      {ads.map((ad) => {
-        return <Ad data={ad} key={ad.aid}/>
-      })}
+      {
+        !adItems.length ?
+          <CircularProgress /> :
+          (<Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+              {adItems.map((ad) => (
+                <Grid key={ad.aid} item xs={12} sm={6}>
+                  <Ad data={ad}/>
+                </Grid>
+              ))}
+          </Grid>
+          )
+      }
     </>
   );
 }
