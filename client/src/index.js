6f3bc6a69cfad106f2ddfca47977d1aa51491eab
Alexander Zarkov <alexander.zarcoff@outlook.com>
2022-01-03 16:16:50 +0200

    set lodash globally

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store'
+import _ from 'lodash';
+global._ = _;
 
 ReactDOM.render(
   <>
